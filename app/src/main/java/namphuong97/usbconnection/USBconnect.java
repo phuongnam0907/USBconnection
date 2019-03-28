6d78a5aea7a69ff80122acb96e6f1260211bc671
phuongnam0907 <phuongnam0907@gmail.com>
2019-03-28 10:32:06 +0700

    Initial commit

---
+++ app/src/main/java/namphuong97/usbconnection/USBconnect.java
@@ -0,0 +1,170 @@
+package namphuong97.usbconnection;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.felhr.usbserial.UsbSerialDevice;
+import com.felhr.usbserial.UsbSerialInterface;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
+/**
+ * Skeleton of an Android Things activity.
+ * <p>
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ *
+ * <pre>{@code
+ * PeripheralManagerService service = new PeripheralManagerService();
+ * mLedGpio = service.openGpio("BCM6");
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+ * mLedGpio.setValue(true);
+ * }</pre>
+ * <p>
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ */
+public class USBconnect extends Activity {
+
+    private static final String TAG = USBconnect.class.getSimpleName();
+    private static final int USB_VENDOR_ID = 0x1908;
+    private static final int USB_PRODUCT_ID = 0x2311;
+
+    private UsbManager usbManager;
+    private UsbDeviceConnection connection;
+    private UsbSerialDevice serialDevice;
+    private String buffer = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usbconnect);
+
+        usbManager = getSystemService(UsbManager.class);
+
+        // Detach events are sent as a system-wide broadcast
+        IntentFilter filter = new IntentFilter(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        registerReceiver(usbDetachedReceiver, filter);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startUsbConnection();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(usbDetachedReceiver);
+        stopUsbConnection();
+    }
+
+    private UsbSerialInterface.UsbReadCallback callback = new UsbSerialInterface.UsbReadCallback() {
+        @Override
+        public void onReceivedData(byte[] data) {
+            try {
+                String dataUtf8 = new String(data, "UTF-8");
+                buffer += dataUtf8;
+                int index;
+                while ((index = buffer.indexOf('\n')) != -1) {
+                    final String dataStr = buffer.substring(0, index + 1).trim();
+                    buffer = buffer.length() == index ? "" : buffer.substring(index + 1);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            onSerialDataReceived(dataStr);
+                        }
+                    });
+                }
+            } catch (UnsupportedEncodingException e) {
+                Log.e(TAG, "Error receiving USB data", e);
+            }
+        }
+    };
+
+    private final BroadcastReceiver usbDetachedReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+                UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                if (device != null && device.getVendorId() == USB_VENDOR_ID && device.getProductId() == USB_PRODUCT_ID) {
+                    Log.i(TAG, "USB device detached");
+                    stopUsbConnection();
+                }
+            }
+        }
+    };
+
+    private void startUsbConnection() {
+        Map<String, UsbDevice> connectedDevices = usbManager.getDeviceList();
+
+        if (!connectedDevices.isEmpty()) {
+            for (UsbDevice device : connectedDevices.values()) {
+                if (device.getVendorId() == USB_VENDOR_ID && device.getProductId() == USB_PRODUCT_ID) {
+                    Log.i(TAG, "Device found: " + device.getDeviceName());
+                    startSerialConnection(device);
+                    return;
+                }
+            }
+        }
+        Log.w(TAG, "Could not start USB connection - No devices found");
+    }
+
+    private void startSerialConnection(UsbDevice device) {
+        Log.i(TAG, "Ready to open USB device connection");
+        connection = usbManager.openDevice(device);
+        serialDevice = UsbSerialDevice.createUsbSerialDevice(device, connection);
+
+        if (serialDevice != null) {
+            if (serialDevice.open()) {
+                serialDevice.setBaudRate(115200);
+                serialDevice.setDataBits(UsbSerialInterface.DATA_BITS_8);
+                serialDevice.setStopBits(UsbSerialInterface.STOP_BITS_1);
+                serialDevice.setParity(UsbSerialInterface.PARITY_NONE);
+                serialDevice.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+                serialDevice.read(callback);
+                Log.i(TAG, "Serial connection opened");
+            } else {
+                Log.w(TAG, "Cannot open serial connection");
+            }
+        } else {
+            Log.w(TAG, "Could not create Usb Serial Device");
+        }
+    }
+
+    private void onSerialDataReceived(String data) {
+        // Add whatever you want here
+        Log.i(TAG, "Serial data received: " + data);
+    }
+
+    private void stopUsbConnection() {
+        try {
+            if (serialDevice != null) {
+                serialDevice.close();
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
+        } finally {
+            serialDevice = null;
+            connection = null;
+        }
+    }
+
+}
